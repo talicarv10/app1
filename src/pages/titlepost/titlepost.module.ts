8e213e1fc2b0b1f0da05f8a3529e99a63490700a
Carvalho <talita.carvalho@capgemini.com>
2019-01-10 14:35:30 -0300

    Alterando layout de página de posts

---
+++ src/pages/titlepost/titlepost.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TitlepostPage } from './titlepost';
+
+@NgModule({
+  declarations: [
+    TitlepostPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TitlepostPage),
+  ],
+})
+export class TitlepostPageModule {}
