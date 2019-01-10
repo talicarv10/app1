1c016984b500358fb177cbab99a12ff35d46f18b
Carvalho <talita.carvalho@capgemini.com>
2019-01-10 08:43:18 -0300

    Adicionando detalhes de layout na página inicial

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AppPage } from '../app/app';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,6 +17,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  openApp(){
+    this.navCtrl.push(AppPage, {});    
   }
 
   ionViewDidLoad() {
