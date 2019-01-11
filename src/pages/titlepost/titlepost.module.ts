1be286d5041b447b1db89066208ba950d5d83226
Carvalho <talita.carvalho@capgemini.com>
2019-01-11 17:42:50 -0300

    Alterando lista de posts

--- src/pages/titlepost/titlepost.module.ts
+++ src/pages/titlepost/titlepost.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TitlepostPage } from './titlepost';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { TitlepostPage } from './titlepost';
   ],
   imports: [
     IonicPageModule.forChild(TitlepostPage),
+    ComponentsModule
   ],
 })
 export class TitlepostPageModule {}
--- src/pages/titlepost/titlepost.ts
+++ src/pages/titlepost/titlepost.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PostComponent } from '../../components/post/post';
 
 /**
  * Generated class for the TitlemsgPage page.
@@ -14,9 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'titlepost.html',
 })
 export class TitlepostPage {
+  
+  public post0 : Array<Object> = [];
+ 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.post0 = [
+
+      {
+        title: "Férias de verão",
+        description: "Minhas férias de verão foram incríveis e blablabla, adorei!",
+        name: "Talita Carvalho",
+        date: new Date()
+      },
+
+      {
+        title: "Férias de inverno",
+        description: "Minhas férias de inverno foram incríveis e blablabla, adorei!",
+        name: "Lorena Lima",
+        date: new Date()
+      },
+
+      {
+        title: "Férias de primavera",
+        description: "Minhas férias de primavera foram incríveis e blablabla, adorei!",
+        name: "Lucas Souza",
+        date: new Date()
+      }
+
+    ]
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TitlepostPage');
