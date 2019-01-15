f6369c58899cef694802516d969f18a4f5fb85eb
Carvalho <talita.carvalho@capgemini.com>
2019-01-15 08:37:52 -0300

    Criando provider

---
+++ src/providers/lastpost/lastpost.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the LastpostProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class LastpostProvider {
+
+  private baseApiPath = "http://aulas.getsandbox.com";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello LastpostProvider Provider');
+  }
+
+getLastpost(){
+  return this.http.get(this.baseApiPath + "/last_post");
+}
+
+}
